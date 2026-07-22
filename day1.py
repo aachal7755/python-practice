d1dd5d7396af3d1c6a63fcc70d90ed161633179a
Aachal Kokate <kokateaachal@gmail.com>
2026-07-23 00:21:22 +0530

    upload day 1 python besics
    uploaded a basic python programs created during day 1 practice

---
+++ day1.py
@@ -0,0 +1,10 @@
+name =input ("enter your name")
+college =input ("enter your college name")
+branch=input("enter your branch")
+
+
+print("Hello" , name)
+print("your college name is",college)
+print("your branch is",branch)
+
+
