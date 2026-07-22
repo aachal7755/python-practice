fa259a78a3296a9fbbafb57ef84f5089928a6096
Aachal Kokate <kokateaachal@gmail.com>
2026-07-23 00:39:12 +0530

    Add day_3 if-else practice
    added a python programs using if , elif , and else condition

---
+++ if else day 3.py
@@ -0,0 +1,9 @@
+marks=int(input("enter your marks"))
+if marks>=90:
+          print("exellent")
+elif marks>=75:
+          print("very good")
+elif marks>=35:
+          print("pass")
+else:
+          print("fail")
