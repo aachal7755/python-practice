11e86ef37b3f606a3860213600205de62ccb1d21
Aachal Kokate <kokateaachal@gmail.com>
2026-07-23 23:22:23 +0530

    Add Day 4 if-else practice programs

---
+++ day4_largest_of_two.py
@@ -0,0 +1,11 @@
+number1=int(input("enter a first number"))
+number2=int(input("enter a second number"))
+if number1>=number2:
+   print("the first number is largest")
+elif number2>=number1:
+    print("the second number is largest")
+    
+else:
+    
+   print("the number is small")
+
---
+++ day4_positive_negative.py
@@ -0,0 +1,7 @@
+number=int(input("enter a number"))
+if number < 0:
+    print("the number is negative")
+elif number > 0:
+    print("the number is positive")
+else:
+    print("the number is zero")
