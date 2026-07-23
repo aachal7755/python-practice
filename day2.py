5ca3754dc15135a98b8728b4096e05a6273523b0
Aachal Kokate <kokateaachal@gmail.com>
2026-07-23 18:35:48 +0530

    Add Day 2 Python arithmetic operations program
    Python Basics - Day 2 Arithmetic Operations Practice

---
+++ day2.py
@@ -0,0 +1,8 @@
+x= int(input("enter a first number:"))
+y= int(input("enter a second number:"))
+print("addition=",x+y)
+print("substraction=",x-y)
+print("division=",x/y)
+print("multiplication=",x*y)
+print("reminder=",x%y)
+      
