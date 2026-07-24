d17d700fb48c7fb9431f2ebea0e7f7cc48528957
Aachal Kokate <kokateaachal@gmail.com>
2026-07-24 23:16:41 +0530

    Add Day 5 conditional statements practice

---
+++ day5_largest_of_three.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter a first number"))
+num2=int(input("Enter a second number"))
+num3=int(input("enter a third number"))
+
+if num1 > num2 and num1 > num3 :
+    print("the first number is largest")
+
+elif num2 > num1 and num2 > num3 :
+    print ("the second number is largest")
+
+else:
+    print("the third number is largest")
