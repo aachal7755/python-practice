985809f85d9d035378751b8b2acf425ed0699034
Aachal Kokate <kokateaachal@gmail.com>
2026-07-28 00:00:22 +0530

    Add Day 8 if-elif-else practice programs

---
+++ day8_grade_calculator.py
@@ -0,0 +1,10 @@
+choice=int(input("enter your choice (1-3)"))
+if choice == 1:
+    print("your balance is 5000")
+elif choice == 2:
+    print("Deposite succesfull")
+elif choice == 3:
+    print("withdrawal succesfull")
+else:
+    print("Invalid choice")
+
---
+++ day8_simple_calculator.py
@@ -0,0 +1,11 @@
+marks=int(input("enter your marks;")
+if marks >= 90:
+        print("Grade A")
+elif marks >= 75 or marks >= 95 :
+        print("Grade B")
+elif marks >= 35 or marks >= 74 :
+        print("Grade c")
+else:
+     marks < 35:
+        print("fail")
+        
